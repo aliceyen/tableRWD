f6cfb8c002e12904b982f2d1580dabe60ade960e
aliceyen <43776397+aliceyen@users.noreply.github.com>
2019-09-20 10:54:45 +0800

    Add files via upload

---
+++ src/fglife_TableRwd/js/pageTable.js
@@ -0,0 +1,30 @@
+$(function () {
+    resizeWindow();
+
+    //視窗縮放
+    $(window).resize(function () {
+        resizeWindow();
+    });
+
+    //水平滾動
+    $('.container-block').scroll(function () {
+        scrollWindow(this);
+    });
+});
+
+function resizeWindow() {
+    $('.container-block').each(function () {
+        if ($(this)[0].scrollWidth > $(this)[0].clientWidth) //有scrollbar時
+            $(this).siblings('.red').addClass('show');
+        else
+            $(this).siblings('.red').removeClass('show');
+    });
+}
+
+function scrollWindow(obj) {
+    var $obj = $(obj);
+    if (($obj.width() + $obj.scrollLeft()) == $obj[0].scrollWidth) //滑到最右邊
+        $obj.siblings('.red').removeClass('show');
+    else
+        $obj.siblings('.red').addClass('show');
+}
